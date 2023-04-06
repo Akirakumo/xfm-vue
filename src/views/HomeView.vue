6eaffb7a36c509d02fb481b671d79d57aba847ce
xmy <616710442@qq.com>
2023-04-06 21:58:36 +0800

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { RouterView } from 'vue-router'
+import { Fold } from '@element-plus/icons-vue'
+import Sider from '../components/Sider.vue'
+
+const components = {
+  Sider
+}
+
+const isCollapse = ref(true)
+const msg = ref('home')
+</script>
+
+<template>
+  <el-container class="common-layout">
+    <el-aside class="aside">
+      <Sider :isCollapse="isCollapse" />
+    </el-aside>
+    <el-container class="content">
+      <el-header class="header">
+        <el-icon>
+          <Fold v-show="!isCollapse" />
+
+        </el-icon>
+      </el-header>
+      <el-main class="main">
+        <RouterView />
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<style scoped>
+.common-layout {
+  height: 100%;
+}
+
+.header {
+  background-color: #6777ef;
+}
+</style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <main>
+        <header>
+            <div class="wrapper">
+                <nav>
+                    <RouterLink to="/home">Home</RouterLink>
+                    <RouterLink to="/about">About</RouterLink>
+                </nav>
+            </div>
+        </header>
+    </main>
+</template>
